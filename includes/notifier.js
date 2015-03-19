71ee5aa0197e748f14f51b56a07b9cc8802144f8
alandarev <alandarev@gmail.com>
2015-03-19 11:04:48 +0000

    Notifier now sends an email. Template is not implemented

--- includes/notifier.js
+++ includes/notifier.js
@@ -1,6 +1,25 @@
+var nodemailer = require('nodemailer'),
+    util = require('util');
+var transport = require('nodemailer-direct-transport');
+
+var transporter = nodemailer.createTransport(transport({debug: true}));
 
 function notify(user, entries)  {
+  var htmlCode = formatEntries(entries);
 
+  transporter.sendMail({
+    from: {name: "Property Finder", address: "properties@alandarev.com"},
+    to: user,
+    subject: util.format("%d new properties found", entries.length),
+    text: "HTML Only",
+    html: htmlCode
+  }, function(err, info)  {
+    if(err) console.error(err);
+  });
+}
+
+function formatEntries(entries) {
+  return "<html><body><b>HELLO WORLD</b></body></html>";
 }
 
 
